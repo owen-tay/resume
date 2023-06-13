05f204fecf35ed495c4d6c56863c94bf546b7c69
Owen Taylor <you@example.com>
2023-06-13 23:43:57 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        red: {
+          myred: '#FF577F',
+        },
+        yellow: {
+          myyellow: '#FFC764',
+        },
+      },
+      screens: {
+        'xs': '300px',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
